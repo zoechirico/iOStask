15af9bd9a9e233e98efbc213d1fc5022203efd7a
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-11-30 09:21:31 -0500

    Initial Commit

---
+++ iOStask/iOStaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOStaskApp.swift
+//  iOStask
+//
+//  Created by Mike Chirico on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct iOStaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
