6f57e2ca3ab8e4ff2aee8a938631d4ab619d8f96
Mike Chirico <mchirico@gmail.com>
2020-12-03 16:19:04 -0500

    Adding icons (#2)

---
+++ iOStask/Credentials/Credentials_test.swift
@@ -0,0 +1,35 @@
+//
+//  Credentials_test.swift
+//  iOStaskTests
+//
+//  Created by Mike Chirico on 12/3/20.
+//
+
+import XCTest
+
+class Credentials_test: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        if let p = p {
+            print("\n\nhere: \(p)")
+        }
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
