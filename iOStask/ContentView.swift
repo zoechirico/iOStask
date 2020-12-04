6fffaf9d0e0d5ad2da49d93c1c7bd4e71867b5fe
Mike Chirico <mchirico@gmail.com>
2020-12-04 08:29:56 -0500

    Simple buttons added (#3)

--- iOStask/ContentView.swift
+++ iOStask/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var txt0: String = "iOStask"
+    @State var txt1: String = "Button"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        Button(action: {
+            self.txt0 = "iOStask"
+            self.txt1 = "BUTTON"
+        }) {
+            Text(self.txt0)
+                .padding(.all,30)
+                .padding([.top,.bottom],20)
+                .background(Color.green)
+                .foregroundColor(Color.black)
+                .cornerRadius(5)
+                .shadow(radius: 25)
+                .font(Font.custom("Avenir-Black", size: 17))
+        }
+        
+        
+        
+        Button(action: {
+            self.txt0 = "* iOStask *"
+            self.txt1 = "button"
+        }) {
+            Text(self.txt1)
+                .padding([.leading,.trailing],20)
+                .padding([.top,.bottom],10)
+                .background(Color.red)
+                .foregroundColor(Color.black)
+                .cornerRadius(15)
+                .shadow(radius: 15)
+                .multilineTextAlignment(.center)
+            
+        }
     }
 }
 
