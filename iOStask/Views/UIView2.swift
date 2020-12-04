39a619a9a3a0ac539372586334e6bdd3b1937be1
Mike Chirico <mchirico@gmail.com>
2020-12-04 09:47:17 -0500

    Adding UIViews (#4)

---
+++ iOStask/Views/UIView2.swift
@@ -0,0 +1,20 @@
+//
+//  UIView2.swift
+//  iOStask
+//
+//  Created by Mike Chirico on 12/4/20.
+//
+
+import SwiftUI
+
+struct UIView2: View {
+    var body: some View {
+        Text("View 2")
+    }
+}
+
+struct UIView2_Previews: PreviewProvider {
+    static var previews: some View {
+        UIView2()
+    }
+}
