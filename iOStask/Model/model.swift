41a36bbaedb2fb495ed57e78ee60bd73887a2134
Mike Chirico <mchirico@gmail.com>
2020-12-06 13:36:58 -0500

    images scrollview (#11)

--- iOStask/Model/model.swift
+++ iOStask/Model/model.swift
@@ -20,7 +20,7 @@ public class DB {
         
         guard let  image = db.img2(color1: UIColor.green,color2: UIColor.blue, size: CGSize(width: 20,height: 20)) else {
             print("Can't create image.")
-
+            
             return
         }
         
@@ -28,7 +28,7 @@ public class DB {
         
         r = db.result()
         
-
+        
         for (_ , item) in r!.enumerated() {
             print("\(item.t1key),\t \(item.data), \(item.num), \(item.timeEnter)")
         }
@@ -36,26 +36,35 @@ public class DB {
         db.close()
     }
     
+    
+    public func GetResults(file: String) -> [S2Result]? {
+        let db = SQLite2(file: file)
+        
+        db.open()
+        db.create()
+        r = db.result()
+        db.close()
+        return r
+    }
+    
+    public func Delete(file: String) {
+        let db = SQLite2(file: file)
+        
+        db.open()
+        db.execute(sql: "DROP TABLE IF EXISTS t0;")
+        db.close()
+        
+    }
+    
+    
     public func completeImage(png: Data) {
         let db = SQLite2(file: "iOStask.sqlite")
         
         db.open()
-        db.execute(sql: "drop table t0;")
         db.create()
         
-        
-        
-        
-        
-        
         db.insert(data: "data a", image: png, num: 17.8)
-        
         r = db.result()
-        
-
-        for (_ , item) in r!.enumerated() {
-            print("\(item.t1key),\t \(item.data), \(item.num), \(item.timeEnter)")
-        }
         
         db.close()
     }
--- iOStask/Views/UIViewPhotos.swift
+++ iOStask/Views/UIViewPhotos.swift
@@ -13,6 +13,8 @@ struct UIViewPhotos: View {
     @State var imageUI:UIImageView?
     @State var resultUIImage:UIImage?
     
+    @State var recs:[UIImage]=[]
+    
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
     
@@ -31,34 +33,82 @@ struct UIViewPhotos: View {
     
     var body: some View {
         
-        if let image = resultUIImage {
-            Image(uiImage: image)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 300,height:300)
-                .shadow(radius: 15)
-            
+        ScrollView(.horizontal){
+            LazyHStack{
+                
+                ForEach(self.recs, id: \.self) {
+                    Image(uiImage: $0)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300,height:300)
+                        .shadow(radius: 15)
+                }
+                
+                
+            }
         }
         
         Text("Select Photo")
         
-        Button(action: {
-            self.showingImagePicker=true
-            
-        }) {
-            Text("Select Image")
-                .padding([.leading,.trailing],20)
-                .padding([.top,.bottom],10)
-                .background(Color.yellow)
-                .foregroundColor(Color.black)
-                .cornerRadius(15)
-                .shadow(radius: 15)
-                .multilineTextAlignment(.center)
-            
-        }.sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: self.$inputImage)
-        }
         
+        
+        VStack {
+            
+            Button(action: {
+                let db = DB()
+                db.Delete(file: "iOStask.sqlite")
+                self.recs.removeAll()
+            }) {
+                Text("Delete All")
+                    .padding([.leading,.trailing],20)
+                    .padding([.top,.bottom],10)
+                    .background(Color.red)
+                    .foregroundColor(Color.yellow)
+                    .cornerRadius(15)
+                    .shadow(radius: 15)
+                    .multilineTextAlignment(.center)
+                
+            }
+            
+            
+            Button(action: {
+                let db = DB()
+                let r = db.GetResults(file: "iOStask.sqlite")
+                self.recs.removeAll()
+                for rec in r! {
+                    recs.append( UIImage(data: rec.image as Data)!)
+                }
+                
+            }) {
+                Text("Show Images from Database")
+                    .padding([.leading,.trailing],20)
+                    .padding([.top,.bottom],10)
+                    .background(Color.yellow)
+                    .foregroundColor(Color.black)
+                    .cornerRadius(15)
+                    .shadow(radius: 15)
+                    .multilineTextAlignment(.center)
+                
+            }
+            
+            Button(action: {
+                self.showingImagePicker=true
+                
+            }) {
+                Text("Select Image")
+                    .padding([.leading,.trailing],20)
+                    .padding([.top,.bottom],10)
+                    .background(Color.yellow)
+                    .foregroundColor(Color.black)
+                    .cornerRadius(15)
+                    .shadow(radius: 15)
+                    .multilineTextAlignment(.center)
+                
+            }.sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                ImagePicker(image: self.$inputImage)
+            }
+            
+        }
     }
 }
 
