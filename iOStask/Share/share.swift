1dcb1189d49c525d065ce55558e0e87ec0476530
Mike Chirico <mchirico@gmail.com>
2020-12-05 19:15:35 -0500

    Better version of sqlitebroker (#6)
    * View2 with menu
    
    * SQLite version 2

---
+++ iOStask/Share/share.swift
@@ -0,0 +1,25 @@
+//
+//  share.swift
+//  iOStask
+//
+//  Created by Mike Chirico on 12/5/20.
+//
+
+import SwiftUI
+@discardableResult
+func share(
+    items: [Any],
+    excludedActivityTypes: [UIActivity.ActivityType]? = nil
+) -> Bool {
+    guard let source = UIApplication.shared.windows.last?.rootViewController else {
+        return false
+    }
+    let vc = UIActivityViewController(
+        activityItems: items,
+        applicationActivities: nil
+    )
+    vc.excludedActivityTypes = excludedActivityTypes
+    vc.popoverPresentationController?.sourceView = source.view
+    source.present(vc, animated: true)
+    return true
+}
--- iOStask/Views/UIView2.swift
+++ iOStask/Views/UIView2.swift
@@ -5,11 +5,85 @@
 //  Created by Mike Chirico on 12/4/20.
 //
 
+
 import SwiftUI
 
+// two
+
+
+
+
+
+// END
+
+
+
+struct RedMenu: MenuStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Menu(configuration)
+            .foregroundColor(.red)
+    }
+}
+
 struct UIView2: View {
+    @State private var isSharePresented: Bool = false
+    @Environment(\.presentationMode) var presentation
+
     var body: some View {
-        Text("View 2")
+        
+
+        
+        
+        NavigationView {
+            
+            
+            
+            Menu {
+                Button("SQLite", action: {
+                        
+                    let sb = SqliteBroker()
+                     sb.myStart()
+                     print("file url: \(sb.getDatabaseFileURL()) ")
+                    
+                    
+                    let textToShare = "SQLite Database"
+                    let url = sb.getDatabaseFileURL()
+                    let objectsToShare = [textToShare, url] as [Any]
+                    
+                    share(items: objectsToShare)
+                })
+                
+                
+                Button("Adjust Order", action: {print("a")})
+                Menu("Advanced") {
+                        Button("Rename", action: {print("r")})
+                        Button("Delay", action: {print("s")})
+                    }
+            } label: {
+                VStack {
+                Label("Options", systemImage: "paperplane")
+                Text("stuff")
+                   
+                }
+            }.padding()
+            .navigationTitle("View2")
+            .background(Color.red)
+            .foregroundColor(Color.black)
+            .cornerRadius(15)
+            .shadow(radius: 15)
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Back") {
+                        self.presentation.wrappedValue.dismiss()
+                    }
+                    
+                    
+                }
+            }
+
+            
+        }
+        
     }
 }
 
