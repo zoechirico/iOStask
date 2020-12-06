dc750782a8897d373966a1ce66bb5f651d4551b0
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-06 07:53:36 -0500

    update-automated-save

---
+++ iOStask/Model/model.swift
@@ -0,0 +1,35 @@
+//
+//  model.swift
+//  iOStask
+//
+//  Created by Mike Chirico on 12/6/20.
+//
+//import Foundation
+
+import UIKit
+public class DB {
+    var r:[S2Result]?
+    
+    public func run() {
+        let db = SQLite2(file: "iOStask.sqlite")
+        db.open()
+        db.create()
+        
+        guard let  image = db.img(color: UIColor.green,size: CGSize(width: 20,height: 20)) else {
+            print("Can't create image.")
+
+            return
+        }
+        
+        db.insert(data: "data a", image: image, num: 17.8)
+        
+        r = db.result()
+        
+
+        for (_ , item) in r!.enumerated() {
+            print("\(item.t1key),\t \(item.data), \(item.num), \(item.timeEnter)")
+        }
+        
+        db.close()
+    }
+}
--- iOStask/Views/UIViewImages.swift
+++ iOStask/Views/UIViewImages.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct UIViewImages: View {
+    @State var image:UIImage?
+    
     var body: some View {
+        
+        if let image = image {
+            Image(uiImage: image)
+                .frame(width: 200, height: 300)
+                .aspectRatio(contentMode: ContentMode.fit)
+        }
         Text("images")
+        Button(action: {
+            let m = DB()
+            m.run()
+            self.image  = UIImage(data: m.r![0].image as Data)!
+            
+        }) {
+            Text("Button")
+                .padding([.leading,.trailing],20)
+                .padding([.top,.bottom],10)
+                .background(Color.yellow)
+                .foregroundColor(Color.black)
+                .cornerRadius(15)
+                .shadow(radius: 15)
+                .multilineTextAlignment(.center)
+            
+        }
+        
+        
     }
 }
 
