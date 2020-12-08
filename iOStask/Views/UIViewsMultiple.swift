ccaa49d4a8054ac19a38512fbd56b02cf1ca6709
Mike Chirico <mchirico@gmail.com>
2020-12-08 11:22:36 -0500

    For each photo with text... (#20)

--- iOStask/Views/UIViewsMultiple.swift
+++ iOStask/Views/UIViewsMultiple.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct UIViewsMultiple: View {
     @State var showingMulti1 = false
     @State var showingMulti2 = false
+    @State var showingMulti3 = false
     
     @Binding var showModal:Bool
     var body: some View {
@@ -51,6 +52,19 @@ struct UIViewsMultiple: View {
                 .shadow(radius: 15)
         }.fullScreenCover(isPresented: $showingMulti2) {
             UIViewMulti2(showModal: $showingMulti2)
+        }
+        
+        Button(action: {
+            self.showingMulti3=true
+        }) {
+            Text("Multi2")
+                .padding([.all],10)
+                .background(Color.yellow)
+                .foregroundColor(Color.black)
+                .cornerRadius(15)
+                .shadow(radius: 15)
+        }.fullScreenCover(isPresented: $showingMulti3) {
+            UIViewMulti3(showModal: $showingMulti3)
         }
         
         
